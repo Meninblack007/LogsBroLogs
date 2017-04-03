fdf9c39210a1eaa99dd27be9300263bd05c8589f
zeeshanhussain <zeeshanhussain12@gmail.com>
2017-04-03 23:55:57 +0530

    LogcatTile: Add a Dialog

--- app/src/main/java/com/abhi/logsbrologs/LogcatTile.java
+++ app/src/main/java/com/abhi/logsbrologs/LogcatTile.java
@@ -1,9 +1,17 @@
 package com.abhi.logsbrologs;
 
 import android.annotation.TargetApi;
-import android.content.Intent;
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Build;
+import android.os.Environment;
 import android.service.quicksettings.TileService;
+import android.widget.Toast;
+
+import java.io.File;
+
+import eu.chainfire.libsuperuser.Shell;
 
 @TargetApi(Build.VERSION_CODES.N)
 
@@ -17,14 +25,48 @@ public class LogcatTile extends TileService {
     public void onStartListening() {
         super.onStartListening();
     }
+    String LOG_FILE = new File(Environment.getExternalStorageDirectory(), "Logsbrologs.txt").getAbsolutePath();
+    String RAM_FILE = new File(Environment.getExternalStorageDirectory(), "Ramoops.txt").getAbsolutePath();
+    String DMESG_FILE =  new File(Environment.getExternalStorageDirectory().getAbsolutePath())+"/Dmesg.txt";
 
+    public Dialog logDialog() {
+        CharSequence options[] = new CharSequence[]{"Logcat", "Ramoops", "Dmesg"};
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Options");
+        alertDialog.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        if(Shell.SU.available()){
+                            Shell.SU.run("logcat -d >" + LOG_FILE);
+                        } else
+                        {
+                            Toast.makeText(LogcatTile.this, "Su permission denied", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case 1:
+                        if(Shell.SU.available()){
+                            Shell.SU.run("cat /sys/fs/pstore/console-ramoops >" + RAM_FILE);
+                        } else {
+                            Toast.makeText(LogcatTile.this, "Su permission denied", Toast.LENGTH_SHORT).show();
+                        } break;
+                    case 2:
+                        if(Shell.SU.available()) {
+                            Shell.SU.run("dmesg >" + DMESG_FILE);
+                        } else {
+                            Toast.makeText(LogcatTile.this, "Su permission denied", Toast.LENGTH_SHORT).show();
+                        }
+
+                        }
+            }
+        });
+        return alertDialog.create();
+    }
     @Override
     public void onClick() {
         super.onClick();
-        Intent collapseIntent = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
-        Intent intent=new Intent(this, MainActivity.class);
-        sendBroadcast(collapseIntent);
-        startActivity(intent);
+        showDialog(logDialog());
     }
 
 }
